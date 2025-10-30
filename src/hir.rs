087dbc4b213ae050ecf2804ed764feabeb1eb7e9
krobbi <70552146+krobbi@users.noreply.github.com>
2025-10-30 18:07:02 +0000

    Lower number and parenthesized expressions

--- src/hir.rs
+++ src/hir.rs
@@ -1,3 +1,19 @@
+#![expect(dead_code, reason = "HIR should be debug printed")]
+
 /// A high level intermediate representation of a program.
 #[derive(Debug)]
-pub struct Hir;
+pub struct Hir(pub Vec<Stmt>);
+
+/// A statement.
+#[derive(Debug)]
+pub enum Stmt {
+    /// A print statement.
+    Print(Box<Expr>),
+}
+
+/// An expression.
+#[derive(Debug)]
+pub enum Expr {
+    /// A number.
+    Number(f64),
+}
--- src/resolver.rs
+++ src/resolver.rs
@@ -1,6 +1,40 @@
-use crate::{ast::Ast, hir::Hir};
+use crate::{
+    ast::{self, Ast},
+    hir::{self, Hir},
+};
 
-/// Validates an [`Ast`] and lowers it to [`Hir`].
-pub fn resolve_ast(_ast: &Ast) -> Hir {
-    Hir
+/// Resolves an [`Ast`] to [`Hir`].
+pub fn resolve_ast(ast: &Ast) -> Hir {
+    let mut stmts = Vec::with_capacity(ast.0.len());
+
+    for stmt in &ast.0 {
+        let stmt = resolve_stmt(stmt);
+        stmts.push(stmt);
+    }
+
+    Hir(stmts)
+}
+
+/// Resolves an [`ast::Stmt`] to an [`hir::Stmt`].
+fn resolve_stmt(stmt: &ast::Stmt) -> hir::Stmt {
+    match stmt {
+        ast::Stmt::Assign(..) => todo!("lowering of `ast::Stmt::Assign`"),
+        ast::Stmt::Expr(expr) => {
+            let expr = resolve_expr(expr);
+            hir::Stmt::Print(expr.into())
+        }
+    }
+}
+
+/// Resolves an [`ast::Expr`] to an [`hir::Expr`].
+fn resolve_expr(expr: &ast::Expr) -> hir::Expr {
+    match expr {
+        ast::Expr::Number(value) => hir::Expr::Number(*value),
+        ast::Expr::Ident(..) => todo!("lowering of `ast::Expr::Ident`"),
+        ast::Expr::Paren(expr) => resolve_expr(expr),
+        ast::Expr::Block(..) => todo!("lowering of `ast::Expr::Block`"),
+        ast::Expr::Call(..) => todo!("lowering of `ast::Expr::Call`"),
+        ast::Expr::Unary(..) => todo!("lowering of `ast::Expr::Unary`"),
+        ast::Expr::Binary(..) => todo!("lowering of `ast::Expr::Binary`"),
+    }
 }
