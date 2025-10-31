32f67146a790a35d2ddfd9f57c996408d66c5e2f
krobbi <70552146+krobbi@users.noreply.github.com>
2025-10-31 14:57:05 +0000

    Compile number and parenthesized expressions

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,6 +1,63 @@
-use crate::{ast::Ast, ir::Ir};
+use crate::{
+    ast::{Ast, Expr, Stmt},
+    ir::{Body, Instruction, Ir, Value},
+};
 
 /// Compiles an [`Ast`] to [`Ir`].
-pub fn compile_ast(_ast: &Ast) -> Ir {
-    Ir
+pub fn compile_ast(ast: &Ast) -> Ir {
+    let mut compiler = Compiler::new();
+    compiler.compile_ast(ast);
+    Ir(compiler.into_body())
+}
+
+/// A structure that compiles a program or function's [`Body`].
+struct Compiler {
+    /// The [`Instruction`]s that have been compiled.
+    instructions: Vec<Instruction>,
+}
+
+impl Compiler {
+    /// Creates a new `Compiler`.
+    fn new() -> Self {
+        let instructions = Vec::new();
+        Self { instructions }
+    }
+
+    /// Consumes the `Compiler` and converts it to a [`Body`].
+    fn into_body(self) -> Body {
+        Body(self.instructions.into_boxed_slice())
+    }
+
+    /// Compiles an [`Ast`].
+    fn compile_ast(&mut self, ast: &Ast) {
+        for stmt in &ast.0 {
+            match stmt {
+                Stmt::Assign(..) => todo!("compilation of `Stmt::Assign`"),
+                Stmt::Expr(expr) => {
+                    self.compile_expr(expr);
+                    self.compile(Instruction::Print);
+                }
+            }
+        }
+
+        self.compile(Instruction::Halt);
+    }
+
+    /// Compiles an [`Expr`].
+    fn compile_expr(&mut self, expr: &Expr) {
+        match expr {
+            Expr::Number(value) => self.compile(Instruction::PushValue(Value::Number(*value))),
+            Expr::Ident(..) => todo!("compilation of `Expr::Ident`"),
+            Expr::Paren(expr) => self.compile_expr(expr),
+            Expr::Block(..) => todo!("compilation of `Expr::Block`"),
+            Expr::Call(..) => todo!("compilation of `Expr::Call`"),
+            Expr::Unary(..) => todo!("compilation of `Expr::Unary`"),
+            Expr::Binary(..) => todo!("compilation of `Expr::Binary`"),
+        }
+    }
+
+    /// Appends an [`Instruction`] to the current block.
+    fn compile(&mut self, instruction: Instruction) {
+        self.instructions.push(instruction);
+    }
 }
--- src/ir.rs
+++ src/ir.rs
@@ -1,3 +1,29 @@
+#![expect(dead_code, reason = "IR code should be debug printed")]
+
 /// An intermediate representation of a program.
 #[derive(Debug)]
-pub struct Ir;
+pub struct Ir(pub Body);
+
+/// A sequence of [`Instruction`]s in a program or function body.
+#[derive(Debug)]
+pub struct Body(pub Box<[Instruction]>);
+
+/// An executable instruction.
+#[derive(Debug)]
+pub enum Instruction {
+    /// Push a constant [`Value`] to the stack.
+    PushValue(Value),
+
+    /// Pop a [`Value`] from the stack and print it.
+    Print,
+
+    /// Halt execution.
+    Halt,
+}
+
+/// A value with a dynamic type.
+#[derive(Debug, Clone)]
+pub enum Value {
+    /// A number.
+    Number(f64),
+}
