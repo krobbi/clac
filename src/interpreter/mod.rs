2da992303dfe89285604c3cd5a0fba9dcdb9fd96
krobbi <70552146+krobbi@users.noreply.github.com>
2025-11-06 11:38:02 +0000

    Create interpreter structure

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -3,77 +3,89 @@ mod interpret_error;
 
 pub use self::{globals::Globals, interpret_error::InterpretError};
 
-use crate::ir::{BinOp, Instruction, Ir, Value};
+use std::{cell::RefCell, rc::Rc};
+
+use crate::ir::{BinOp, Body, Instruction, Ir, Value};
 
 /// Interprets [`Ir`] with [`Globals`]. This function returns an
 /// [`InterpretError`] if [`Ir`] could not be interpreted.
 pub fn interpret_ir(ir: &Ir, globals: &mut Globals) -> Result<(), InterpretError> {
-    let mut stack = Stack::new();
+    let mut interpreter = Interpreter::new(&ir.0, RefCell::new(globals).into());
+    interpreter.run()
+}
 
-    for instruction in &ir.0.0 {
-        match instruction {
-            Instruction::Push(value) => stack.push(value.clone()),
-            Instruction::Drop => {
-                stack.pop();
-            }
-            Instruction::Print => println!("{}", stack.pop()),
-            Instruction::LoadLocal(index) => stack.push(stack.get_local(*index).clone()),
-            Instruction::LoadGlobal(name) => stack.push(globals.get(name).clone()),
-            Instruction::StoreLocal(index) => {
-                let value = stack.pop();
-                stack.set_local(*index, value);
-            }
-            Instruction::StoreGlobal(name) => globals.set(name, stack.pop()),
-            Instruction::Binary(op) => {
-                let rhs = stack.pop_number()?;
-                let lhs = stack.pop_number()?;
+/// A structure that interprets a [`Body`].
+struct Interpreter<'a, 'b: 'a> {
+    /// The [`Body`] to interpret.
+    body: &'a Body,
 
-                let result = match op {
-                    BinOp::Add => lhs + rhs,
-                    BinOp::Subtract => lhs - rhs,
-                    BinOp::Multiply => lhs * rhs,
-                    BinOp::Divide => {
-                        if rhs.is_subnormal() || rhs == 0.0 {
-                            return Err(InterpretError::DivideByZero);
-                        }
+    /// The [`Globals`].
+    globals: Rc<RefCell<&'b mut Globals>>,
 
-                        lhs / rhs
-                    }
-                };
+    /// The stack of [`Value`]s.
+    stack: Vec<Value>,
+}
 
-                stack.push(Value::Number(result));
-            }
-            Instruction::Call(..) => todo!("interpreting `Instruction::Call`"),
+impl<'a, 'b> Interpreter<'a, 'b> {
+    /// Creates a new `Interpreter` from its [`Body`] and [`Globals`].
+    fn new(body: &'a Body, globals: Rc<RefCell<&'b mut Globals>>) -> Self {
+        let stack = Vec::new();
+        Self {
+            body,
+            globals,
+            stack,
         }
     }
 
-    Ok(())
-}
+    /// Runs the `Interpreter` until it is finished. This function returns an
+    /// [`InterpretError`] if an error occurred.
+    fn run(&mut self) -> Result<(), InterpretError> {
+        for instruction in &self.body.0 {
+            match instruction {
+                Instruction::Push(value) => self.stack.push(value.clone()),
+                Instruction::Drop => self.stack.truncate(self.stack.len() - 1),
+                Instruction::Print => println!("{}", self.pop()),
+                Instruction::LoadLocal(index) => self.stack.push(self.stack[*index].clone()),
+                Instruction::LoadGlobal(name) => {
+                    self.stack.push(self.globals.borrow().get(name).clone());
+                }
+                Instruction::StoreLocal(index) => self.stack[*index] = self.pop(),
+                Instruction::StoreGlobal(name) => {
+                    let value = self.pop();
+                    self.globals.borrow_mut().set(name, value);
+                }
+                Instruction::Binary(op) => {
+                    let rhs = self.pop_number()?;
+                    let lhs = self.pop_number()?;
 
-/// A stack of [`Value`]s.
-struct Stack {
-    /// The [`Value`]s.
-    values: Vec<Value>,
-}
+                    let result = match op {
+                        BinOp::Add => lhs + rhs,
+                        BinOp::Subtract => lhs - rhs,
+                        BinOp::Multiply => lhs * rhs,
+                        BinOp::Divide => {
+                            if rhs.is_subnormal() || rhs == 0.0 {
+                                return Err(InterpretError::DivideByZero);
+                            }
 
-impl Stack {
-    /// Creates a new `Stack`.
-    fn new() -> Self {
-        let values = Vec::new();
-        Self { values }
+                            lhs / rhs
+                        }
+                    };
+
+                    self.stack.push(Value::Number(result));
+                }
+                Instruction::Call(..) => todo!("interpreting `Instruction::Call`"),
+            }
+        }
+
+        Ok(())
     }
 
-    /// Pushes a [`Value`] to the `Stack`.
-    fn push(&mut self, value: Value) {
-        self.values.push(value);
-    }
-
-    /// Pops a [`Value`] from the `Stack`.
+    /// Pops a [`Value`] from the stack.
     fn pop(&mut self) -> Value {
-        self.values.pop().expect("stack should not be empty")
+        self.stack.pop().expect("stack should not be empty")
     }
 
-    /// Pops a number [`Value`] from the `Stack` and returns its underlying
+    /// Pops a number [`Value`] from the stack and returns its underlying
     /// [`f64`]. This function returns an [`InterpretError`] if the [`Value`] is
     /// not a number.
     fn pop_number(&mut self) -> Result<f64, InterpretError> {
@@ -82,15 +94,5 @@ impl Stack {
         } else {
             Err(InterpretError::InvalidType)
         }
-    }
-
-    /// Returns a reference to a local variable's [`Value`].
-    fn get_local(&self, index: usize) -> &Value {
-        &self.values[index]
-    }
-
-    /// Sets a local variable's [`Value`].
-    fn set_local(&mut self, index: usize, value: Value) {
-        self.values[index] = value;
     }
 }
